019daf863c46569f63e2a7b825faf5482884ed7b
sathiyavenkatesh9086 <68427332+sathiyavenkatesh9086@users.noreply.github.com>
2020-10-18 14:00:04 +0530

    files uploaded

--- script.js
+++ script.js
@@ -9,6 +9,16 @@
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
 
+function checkUserName(uname, mx, my) {
+    var uid_name=uname.value.length
+    if(uid_name==0)
+    {
+        alert("Username should not be empty/ length be between " + mx + " to " + my)
+
+       return false;
+    }
+    return true;
+}
 // Progression 2 
 // Check mail
 // create a function called as checkEmail()
@@ -16,9 +26,31 @@
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
 
+function checkEmail(uemail) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+if(uemail.value.match(mailformat))
+{
+    return true
+}
+else {
+    alert("You have entered an invalid email address!");
+    uemail.focus();
+    return false;
+}
+}
 // Progression 3
 // Check password
 // create a function called as checkPassword()
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+
+function checkPassword(passid, mx, my) {
+    var passid_len = passid.value.length;
+    if (passid_len == 0 || passid_len <= mx || passid_len > my) {
+        alert("Password should not be empty / length be between " + mx + " to " + my);
+        passid.focus();
+        return false;
+    }
+    return true;
+}
